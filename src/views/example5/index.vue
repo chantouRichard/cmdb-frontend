ed10f2bccf9037e3711a0b3132bbc45c4f0f4c8a
chantouRichard <2897167455@qq.com>
2026-05-16 11:55:08 +0800

    fix: resolve eslint and vue template parsing errors

--- src/views/example5/index.vue
+++ src/views/example5/index.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="DashBoard" style="height: 100%;">
     <iframe
-      src="https://apps.ce.bktencent.com/bk-vision/embed/?uid=3UatyVhAx3Qe656cGBAjEz&bk_app_id=&bk_app_list=['whu-chentao', 'chantao20260413']&name=&show_copyright=True&watermark=True&time_readonly=False&show_time=True&show_refresh=True&start_time=now/d&end_time=now/d&preview=False&hide_toolbox=False&hide_filter=False&panels=&refresh=False"
+      :src="iframeUrl"
       style="height: 100%; width: 100%; border: none;">
     </iframe>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   name: 'DashBoard',
-  components: {},
   data() {
     return {
-      initDateTime: new Date(),
+      // 将复杂的 URL 提取到 data 中，通过 :src 动态绑定，完美避开 HTML 解析报错
+      iframeUrl: "https://apps.ce.bktencent.com/bk-vision/embed/?uid=3UatyVhAx3Qe656cGBAjEz&bk_app_id=&bk_app_list=['whu-chentao', 'chantao20260413']&name=&show_copyright=True&watermark=True&time_readonly=False&show_time=True&show_refresh=True&start_time=now/d&end_time=now/d&preview=False&hide_toolbox=False&hide_filter=False&panels=&refresh=False"
     };
-  },
-  created() {},
-  methods: {},
+  }
 };
 </script>
-
